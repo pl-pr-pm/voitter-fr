32d1a6e5cb0478ee8ca5434e9e56f1ca7f8dbafc
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-03-11 01:01:50 +0900

    自動スクロール途中

--- src/features/post/Timeline.tsx
+++ src/features/post/Timeline.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { RefObject, useEffect, useState, useCallback } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-// import styles from "./Post.module.css";
+import styles from "./Timeline.module.css";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -13,10 +13,11 @@ import {
   selectCurrentTrackIndex,
   setCurrentTrackIndex,
   selectUserInfo,
+  selectCurrentRef,
 } from "./timelineSlice";
 import { Tweet } from "./Tweet";
 import { TimelineUserInfo } from "./TimelineUserInfo";
-import { Box } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 
 // import { PROPS_POST } from "../types";
 
@@ -44,6 +45,32 @@ export const Timeline: React.FC = () => {
   const timelines = useSelector(selectTimeline);
   const currentTrackIndex = useSelector(selectCurrentTrackIndex);
   const userInfo = useSelector(selectUserInfo);
+  // const ref = React.createRef<HTMLDivElement>();
+  // const [ref, setRef] = useState<RefObject<HTMLDivElement>>(
+  //   React.createRef<HTMLDivElement>()
+  // );
+  // const ref = useSelector(selectCurrentRef);
+  // このコールバックを呼び出して ref.current.scrollIntoView() を呼び出してスクロール
+  // const scrollToBottomOfList = useCallback(() => {
+  //   ref!.current!.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "end",
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [currentTrackIndex]);
+
+  const scrollToRef = (
+    index: number,
+    currentTrackIndex: number,
+    ref: React.RefObject<HTMLDivElement>
+  ) => {
+    console.log("index", index);
+    console.log("currentTrackIndex", currentTrackIndex);
+    console.log(ref);
+    if (index === currentTrackIndex + 2) {
+      ref && ref.current && ref.current.scrollIntoView();
+    }
+  };
 
   const handleClickProvious = () => {
     const index =
@@ -63,30 +90,56 @@ export const Timeline: React.FC = () => {
       if (currentTrackIndex !== timelines.length - 1) {
         const index = currentTrackIndex + 1;
         dispatch(setCurrentTrackIndex(index));
+        // console.log(ref);
+        // scrollToBottomOfList();
       }
     }, 1200); //1200ms 感覚で再生
   };
   return (
     <>
-      <AudioPlayer
-        src={timelines[currentTrackIndex].tweetContent.voiceUrl}
-        autoPlayAfterSrcChange={init ? false : true} // timlineをselectしたタイミングで再生しないように制御
-        showSkipControls={true}
-        showJumpControls={false}
-        onClickPrevious={() => handleClickProvious()}
-        onClickNext={() => handleClickNext()}
-        onEnded={() => handleTrackEnd()}
-      />
-      <Box>
-        {timelines.map((timeline, index) => (
-          <Tweet
-            index={index}
-            tweetText={timeline.tweetContent.tweetText}
-            createdAt={timeline.tweetContent.createdAt}
-          />
-        ))}
+      <Box
+        component="div"
+        className={styles.timeline_container}
+        m={5}
+        p={1}
+        height="50%"
+        justify-content="space-around"
+        // background-color="#dddddd"
+      >
+        {/* <Box component="div" width="65%"> */}
+        <Grid container direction="column" justifyContent="center" spacing={2}>
+          {timelines.map((timeline, index) => (
+            <Grid item>
+              <Tweet
+                index={index}
+                tweetText={timeline.tweetContent.tweetText}
+                createdAt={timeline.tweetContent.createdAt}
+                setRef={scrollToRef}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        {/* </Box> */}
+        {userInfo.username ? (
+          <Box component="div" ml={2} width="35%" height="75%">
+            <TimelineUserInfo />
+          </Box>
+        ) : (
+          <div></div>
+        )}
       </Box>
-      {userInfo.username ? <TimelineUserInfo /> : <div></div>}
+
+      <Box className={styles.timeilne_player}>
+        <AudioPlayer
+          src={timelines[currentTrackIndex].tweetContent.voiceUrl}
+          autoPlayAfterSrcChange={init ? false : true} // timlineをselectしたタイミングで再生しないように制御
+          showSkipControls={true}
+          showJumpControls={false}
+          onClickPrevious={() => handleClickProvious()}
+          onClickNext={() => handleClickNext()}
+          onEnded={() => handleTrackEnd()}
+        />
+      </Box>
     </>
   );
 };
--- src/features/post/Tweet.tsx
+++ src/features/post/Tweet.tsx
@@ -1,17 +1,50 @@
 import { Box, Paper } from "@material-ui/core";
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCurrentTrackIndex, selectCurrentTrackIndex } from "./timelineSlice";
+import {
+  setCurrentTrackIndex,
+  selectCurrentTrackIndex,
+  setCurrentRef,
+  selectCurrentRef,
+} from "./timelineSlice";
 import styles from "./Tweet.module.css";
 export const Tweet: React.FC<{
   index: number;
   tweetText: string;
   createdAt: string;
-}> = ({ index, tweetText, createdAt }) => {
+  setRef: (
+    index: number,
+    currentTrackIndex: number,
+    ref: React.RefObject<HTMLDivElement>
+  ) => void;
+}> = ({ index, tweetText, createdAt, setRef }) => {
   const dispatch = useDispatch();
   const currentTrackIndex = useSelector(selectCurrentTrackIndex);
+  // const ref = useSelector(selectCurrentRef);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("in callback");
+    if (index === currentTrackIndex + 2) {
+      setRef(index, currentTrackIndex, ref);
+    }
+    // if (index === currentTrackIndex + 1) {
+    //   setRef(ref);
+    //   console.log(ref);
+    // }
+  }, [index, currentTrackIndex, ref]);
+
+  // {index !== (currentTrackIndex + 1) &&
+  //   <div ref={ref}></div>
+  // }
   return (
     <>
+      {index === currentTrackIndex + 2 ? (
+        <div id="scroll" ref={ref}></div>
+      ) : (
+        <div></div>
+      )}
+      {/* <div ref={ref}></div> */}
       {tweetText ? (
         <Paper
           className={
