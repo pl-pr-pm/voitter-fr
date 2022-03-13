7d9e0b6fb75bae37327108d017bc86059f2cbea4
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-03-13 22:36:42 +0900

    ツイッターへのリンク追加

--- src/features/post/TimelineUserInfo.tsx
+++ src/features/post/TimelineUserInfo.tsx
@@ -7,10 +7,13 @@ import { selectUserInfo } from "./timelineSlice";
 
 export const TimelineUserInfo = () => {
   const userInfo = useSelector(selectUserInfo);
+  const targetUrl = `https://twitter.com/${userInfo.username}`;
   return (
     <Card className="userinfoContiener">
       <CardContent>
-        <Avatar src={userInfo.profile_image_url} />
+        <a href={targetUrl}>
+          <Avatar src={userInfo.profile_image_url} />
+        </a>
         <Typography component="h2">{userInfo.username}</Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {userInfo.description}
